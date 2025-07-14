aaf3d0940ce56233d358fe3d688d1b4a10fe146c
Ahmed Mohamed Saad <medosaad2071@gmail.com>
2025-07-14 12:16:32 +0300

    Time: 0 ms (100%), Space: 41.6 MB (49.43%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -21,57 +21,9 @@ class Solution {
         // return -1;
 
         // Using substring
-        // for (int i=0; i<haystack.length()-needle.length()+1; i++) {
-        //     if (haystack.substring(i,i+needle.length()).equals(needle)) return i;
-        // }
-        // return -1;
-
-                if (needle.isEmpty()) return 0;
-
-        int[] lps = buildLPS(needle);
-
-        int i = 0; // haystack index
-        int j = 0; // needle index
-
-        while (i < haystack.length()) {
-            if (haystack.charAt(i) == needle.charAt(j)) {
-                i++;
-                j++;
-                if (j == needle.length()) {
-                    return i - j;
-                }
-            } else {
-                if (j > 0) {
-                    j = lps[j - 1];
-                } else {
-                    i++;
-                }
-            }
+        for (int i=0; i<haystack.length()-needle.length()+1; i++) {
+            if (haystack.substring(i,i+needle.length()).equals(needle)) return i;
         }
-
         return -1;
-    }
-
-        private int[] buildLPS(String needle) {
-        int[] lps = new int[needle.length()];
-        int len = 0;
-        int i = 1;
-
-        while (i < needle.length()) {
-            if (needle.charAt(i) == needle.charAt(len)) {
-                len++;
-                lps[i] = len;
-                i++;
-            } else {
-                if (len > 0) {
-                    len = lps[len - 1];
-                } else {
-                    lps[i] = 0;
-                    i++;
-                }
-            }
-        }
-
-        return lps;
     }
 }

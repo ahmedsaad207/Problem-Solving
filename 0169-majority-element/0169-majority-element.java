3abf289cd40d4e40263c69f1bf7c1e3b4ba96675
Ahmed Mohamed Saad <medosaad2071@gmail.com>
2025-07-14 11:08:59 +0300

    Time: 2 ms (73.81%), Space: 53.4 MB (29.27%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,17 +1,26 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        
 
-        Map<Integer, Integer> map = new HashMap<>();
-        int n = nums.length;
+        // Map<Integer, Integer> map = new HashMap<>();
+        // int n = nums.length;
+
+        // for (int i : nums) {
+        //     map.put(i, map.getOrDefault(i, 0) + 1);
+
+        //     if (map.get(i) > n / 2)
+        //         return i;
+        // }
+
+        // return -1;
+
+        // boyer moore voting algorithm
+        int count = 0, candidate = 0;
 
         for (int i : nums) {
-            map.put(i, map.getOrDefault(i, 0) + 1);
-
-            if (map.get(i) > n/2) return i;
+            if (count == 0) candidate = i;
+            count += (i == candidate) ? 1 : -1;
         }
-
-        return -1;
+        return candidate;
 
     }
 }

a38b07ba3b9b2532a82bee646031586c0ceae1f7
Ahmed Mohamed Saad <medosaad2071@gmail.com>
2025-07-28 22:37:20 +0300

    Time: 2 ms (98.5%), Space: 43 MB (76.52%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,24 +1,49 @@
 class Solution {
     public boolean isPalindrome(String s) {
 
-        StringBuilder result = new StringBuilder();
+        // Solution 1, Add extra space
+        // StringBuilder result = new StringBuilder();
 
-        for (int i=0; i<s.length(); i++) {
-            char c = s.charAt(i);
+        // for (int i=0; i<s.length(); i++) {
+        //     char c = s.charAt(i);
 
+        //     if (Character.isLetterOrDigit(c)) {
+        //         result.append(Character.toLowerCase(c));
+        //     }
+        // }
 
-            if (Character.isLetter(c)) {
-                result.append(Character.toLowerCase(c));
-            } else if (Character.isDigit(c)) {
-                result.append(c);
-            }
+        // for(int i=0; i<result.length()/2; i++) {
+        //     if (result.charAt(i) != result.charAt(result.length()-i-1)) return false;
+        // }
+
+        // return true;
+
+        // Solution 2 - no extra space
+        int l=0,r=s.length()-1;
+
+        while (l<r) {
+            
+            while (l<r && !isLetterOrDigit(s.charAt(l))) l++;
+            while (l<r && !isLetterOrDigit(s.charAt(r))) r--;
+
+            if (toLowerCase(s.charAt(l)) != toLowerCase(s.charAt(r))) return false;
+            l++;
+            r--;
         }
-
-        for(int i=0; i<result.length()/2; i++) {
-            if (result.charAt(i) != result.charAt(result.length()-i-1)) return false;
-        }
-
         return true;
         
+    }
+
+    boolean isLetterOrDigit(char c) {
+        return (c >= 'a' && c <= 'z') || 
+                 c >= 'A' && c <= 'Z' || 
+                 c >= '0' && c <= '9';
+    }
+
+    char toLowerCase(char c) {
+        if (c >= 'A' && c <= 'Z') {
+            return (char)(c+32);
+        }
+        return c;
     }
 }
